68126e37570f5bc3e3fd7539c66f455da77e2640
dotula <dotunglamnd@gmail.com>
2020-12-29 13:50:35 +0700

    app.py fixed 2

--- app.py
+++ app.py
@@ -48,4 +48,4 @@ def image(data_image):
 if __name__ == '__main__':
     print("app is  running")
     # socketio.run(app, host='0.0.0.0', port=3000)
-    socketio.run(app, host="0.0.0.0", debug=True, logger=True, keyfile="key.pem", certfile="cert.pem")
+    socketio.run(app, host="0.0.0.0", debug=True,  keyfile="key.pem", certfile="cert.pem")
