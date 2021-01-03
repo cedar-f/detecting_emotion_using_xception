f2c76b29f793eb885fa6699d8155af41aa28c200
cedar-f <dotunglamnd@gmail.com>
2021-01-03 10:52:14 +0700

    rgba cannot save as jpeg solved

--- img_2_directory.py
+++ img_2_directory.py
@@ -12,12 +12,12 @@ base_directory = 'img/'
 
 def upload_img(img_data, bucket='emtion-img-data', object_name=None):
     if object_name is None:
-        object_name = base_directory + str(datetime.datetime.now()) + get_random_string(4) + '.jpg'
+        object_name = base_directory + str(datetime.datetime.now()) + get_random_string(4) + '.png'
         object_name = object_name.replace(' ', '')
     s3_client = boto3.client('s3')
     try:
         buffer = io.BytesIO()
-        img_data.save(buffer, "JPEG")
+        img_data.save(buffer, "PNG")
         buffer.seek(0)
         s3_client.upload_fileobj(buffer, bucket, object_name, ExtraArgs={'ACL': 'public-read'})
     except ClientError as e:
